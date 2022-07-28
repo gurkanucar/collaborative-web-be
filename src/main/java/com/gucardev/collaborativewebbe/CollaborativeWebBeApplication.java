727b37edd49e75b7b631e3982d0f60f2bc71e8ca
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-28 20:15:27 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/collaborativewebbe/CollaborativeWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.collaborativewebbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborativeWebBeApplication.class, args);
+    }
+
+}
