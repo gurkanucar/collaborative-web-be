5aa0d6ae1d2f8ac026d71311b4fade08081b3c80
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-29 11:11:16 +0300

    default project values constant created

---
+++ src/main/java/com/gucardev/collaborativewebbe/constant/DefaultProjectValues.java
@@ -0,0 +1,9 @@
+package com.gucardev.collaborativewebbe.constant;
+
+public class DefaultProjectValues {
+
+    public static String HTML="<h1 style=\"color:white;\" onclick=\"myFunction()\">Hello! Change Me</h1>";
+    public static String CSS="body{background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 41%, rgba(0,212,255,1) 100%); text-align:center;}";
+    public static String JS="function myFunction() { alert(1)}";
+
+}
